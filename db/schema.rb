bebf240af8b5138e07dbeff0c2ec683233f9af71
Francis Bautista <francis.bautista07@gmail.com>
2015-01-07 22:26:02 +0800

    Added migrations for dashboard module

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150107131654) do
+ActiveRecord::Schema.define(version: 20150107140258) do
+
+  create_table "guardianships", force: true do |t|
+    t.integer  "payment_id"
+    t.integer  "student_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "parents", force: true do |t|
     t.string   "first_name"
