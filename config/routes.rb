83dc9f21983d180dfadeab356c4affe77d9b969b
Francis Bautista <francis.bautista07@gmail.com>
2015-01-10 23:26:44 +0800

    Reset routes for tests

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,15 @@ Rails.application.routes.draw do
 
   resources :guardianships
 
-  devise_for :users
-  resources :parents
-
-  resources :students
 
   resources :payments
 
   resources :tests
+  devise_for :users
+  resources :parents
+
+  resources :students
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
