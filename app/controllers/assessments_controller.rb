0aeae18542109184a87bec15a13a7576e6ba8ff1
Francis Bautista <francis.bautista07@gmail.com>
2015-01-29 11:08:26 +0800

    Demarkation of calculated model data

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -9,10 +9,8 @@ class AssessmentsController < InheritedResources::Base
     # GET /assessments/1
     # GET /assessments/1.json
     def show
-
         @assessment = Assessment.find(params[:id])
-        @discounted_tuition = @assessment.tuition * ((100-@assessment.discount)*0.01)
-
+        # @discounted_tuition = @assessment.tuition * ((100-@assessment.discount)*0.01)
     end
 
     # GET /assessments/new
