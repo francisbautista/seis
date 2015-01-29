3ff370addaba77b8d5f3a404b66c7d6ed27dbc98
Francis Bautista <francis.bautista07@gmail.com>
2015-01-29 11:06:02 +0800

    Removed tuition data from assessments

--- db/migrate/20150121185954_create_assessments.rb
+++ db/migrate/20150121185954_create_assessments.rb
@@ -3,9 +3,9 @@ class CreateAssessments < ActiveRecord::Migration
     create_table :assessments do |t|
       t.string :installment
       t.string :mode
-      t.decimal :tuition
-      t.decimal :discount
-      t.text :discount_desc
+      # t.decimal :tuition
+      # t.decimal :discount
+      # t.text :discount_desc
       t.integer :installment_number
       t.integer :or_number
       t.integer :student_id
