73241ee8cd95dbe4e94d70bb5a2b8fdfada27a07
Francis Bautista <francis.bautista07@gmail.com>
2015-02-05 02:31:03 +0800

    Demarked reservations model

--- db/migrate/20150121190134_create_reservations.rb
+++ db/migrate/20150121190134_create_reservations.rb
@@ -2,7 +2,7 @@ class CreateReservations < ActiveRecord::Migration
   def change
     create_table :reservations do |t|
       t.string :remarks
-      t.decimal :amount
+      # t.decimal :amount
       t.integer :student_id
       t.integer :or_number
       t.boolean :paid_status
