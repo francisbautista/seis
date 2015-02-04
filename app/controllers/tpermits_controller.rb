e82401b4a212dc55c566cd7b2c04cfd2f3e88052
Francis Bautista <francis.bautista07@gmail.com>
2015-02-05 02:32:07 +0800

    Demarked tpermit view

--- app/controllers/tpermits_controller.rb
+++ app/controllers/tpermits_controller.rb
@@ -18,7 +18,7 @@ class TpermitsController < InheritedResources::Base
   private
 
     def tpermit_params
-      params.require(:tpermit).permit(:date, :level, :test_date, :test_number, :status, :remarks, :report_card, :moral_cert, :recommendation, :student_id, :or_number, :amount, :paid_status)
+      params.require(:tpermit).permit(:date, :level, :test_date, :test_number, :status, :remarks, :report_card, :moral_cert, :recommendation, :student_id, :or_number,, :paid_status)
     end
     def set_student
         @student = Student.find(params[:student_id])
