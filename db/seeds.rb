ede0b0853a4c24a4e618eff07984b1f2d6499095
Francis Bautista <francis.bautista07@gmail.com>
2015-02-12 08:33:09 +0800

    added seeds data for user role testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(first_name: 'Yuri', last_name: 'Kristoff',
-email: 'yuri@gmail.com', password: 'password')
+User.create!(first_name: 'Francis', last_name: 'Bautista',
+email: 'francis@gmail.com', password: 'password', position: 'Chief-Registrar')
+
+User.create!(first_name: 'Stuart', last_name: 'Samson',
+email: 'stuart@gmail.com', password: 'password', position: 'Registrar')
+
+User.create!(first_name: 'Bea', last_name: 'Francisco',
+email: 'bea@gmail.com', password: 'password', position: 'Accountant')
 
 
 30.times { Fabricate(:student) }
