8b64908bb6f1e0f889d5ff4833a1a28c3fd0937d
Francis Bautista <francis.bautista07@gmail.com>
2015-02-24 09:15:03 +0800

    Added cache busting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_filter :set_no_cache
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
+
+  def set_no_cache
+      response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+      response.headers["Pragma"] = "no-cache"
+      response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
 
   def configure_devise_permitted_parameters
       registration_params = [:first_name, :middle_name, :last_name, :username, :position, :email, :password, :password_confirmation]
